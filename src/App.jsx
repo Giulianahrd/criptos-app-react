ba5871039f9a188efaed50feceb9e347baea43d2
giuliana <herediagiuli016@gmail.com>
2024-02-21 12:22:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import styled from "@emotion/styled";
+
+const Heading = styled.h1`
+  color: #FFF 
+`
+
+function App() {
+
+  return (
+    <>
+      <Heading>Desde App</Heading>
+    </>
+  )
+}
+
+export default App
